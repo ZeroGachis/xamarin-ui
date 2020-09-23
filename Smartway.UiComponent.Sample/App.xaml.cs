c4e592a95eb54de2d0cabeefce9a1355bd996034
François PERIN <f.perin@zero-gachis.com>
2020-09-23 19:02:05 +0200

    :tada: Add project structure

---
+++ Smartway.UiComponent.Sample/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace Smartway.UiComponent.Sample
+{
+    public partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
