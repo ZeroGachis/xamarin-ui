0eab47ff16e4b28abd55d25374a1c25b79493cf3
a-bertrand <a.bertrand@zero-gachis.com>
2020-10-20 09:54:35 +0200

    :sparkles: add section sheet layout

---
+++ Smartway.UiComponent.Sample/SectionSheet/SectionSheetSample.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Sample.SectionSheet
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SectionSheetSample
+    {
+        public SectionSheetSample()
+        {
+            InitializeComponent();
+        }
+    }
+}
