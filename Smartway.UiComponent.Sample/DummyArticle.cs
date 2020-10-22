000d6430045c80dc52ede154d843408dec4bcde9
m.bazire <m.bazire@zero-gachis.com>
2020-10-22 16:22:40 +0200

    ✨ add components

---
+++ Smartway.UiComponent.Sample/DummyArticle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Smartway.UiComponent.Sample
+{
+    class DummyArticle
+    {
+        public string Label { get; set; }
+        public bool IsMultilocation { get; set; }
+
+        public bool IsOnShortage { get; set; }
+        public string Price { get; set; }
+        public string Gencode { get; set; }
+        public ICommand NavigationCommand => new Command((param) => {
+            var message = "Tapped on " + param.ToString();
+            DependencyService.Get<INotifyMessage>().ShortAlert(message);
+        });
+        public object NavigationParameter { get; set; }
+        public string Status { get; set; }
+        public DateTime ShortageAt => DateTime.Today;
+    }
+}
---
+++ Smartway.UiComponent/CardLists/ArticleCardList.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.CardLists
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ArticleCardList
+    {
+        public static readonly BindableProperty ArticlesListProperty = BindableProperty.Create(nameof(ArticlesList), typeof(List<object>), typeof(ArticleCardList));
+
+        public List<object> ArticlesList
+        {
+            get => (List<object>) GetValue(ArticlesListProperty);
+            set => SetValue(ArticlesListProperty, value);
+        }
+
+        public ArticleCardList()
+        {
+            InitializeComponent();
+            ArticleListElement.PropertyChanged += ArticleListChanged;
+        }
+
+        private void ArticleListChanged(object sender, PropertyChangedEventArgs e)
+        {
+            OnPropertyChanged(nameof(ArticlesList));
+            var element = ArticleListElement.Children.Cast<StackLayout>().FirstOrDefault(_ => _.IsVisible);
+            if (element != null)
+            {
+                element.Children.First().IsVisible = false;
+            }
+        }
+    }
+}
