357db6c85aacacdd312a1f3ad1aafe75cfc5011d
Balou <ewen.balouin@imie.fr>
2020-10-22 17:06:40 +0200

    :sparkles: create sample for top app bar

---
+++ Smartway.UiComponent.Sample/TopAppBar/Views/TopAppBar.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Sample.TopAppBar.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TopAppBar : ContentPage
+    {
+        public TopAppBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
