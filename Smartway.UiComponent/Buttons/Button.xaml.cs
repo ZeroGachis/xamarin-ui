cc6c1b8eb115b39067ead70ca8ff9703cec8836d
Balou <ewen.balouin@imie.fr>
2020-10-28 18:10:56 +0100

    :sparkles: add size and status for button

---
+++ Smartway.UiComponent/Buttons/Button.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Buttons
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Button
+    {
+        public static readonly BindableProperty SizeProperty = BindableProperty.Create(nameof(Size), typeof(string), typeof(Button), "Large");
+        public static readonly BindableProperty StatusProperty = BindableProperty.Create(nameof(Status), typeof(string), typeof(Button), "Primary");
+        public static readonly BindableProperty ActivatedProperty = BindableProperty.Create(nameof(Activated), typeof(bool), typeof(Button), true);
+
+        public string Size
+        {
+            get => (string)GetValue(SizeProperty);
+            set => SetValue(SizeProperty, value);
+        }
+        public string Status
+        {
+            get => (string)GetValue(StatusProperty);
+            set => SetValue(StatusProperty, value);
+        }
+        public bool Activated
+        {
+            get => (bool)GetValue(ActivatedProperty);
+            set => SetValue(ActivatedProperty, value);
+        }
+
+        public Button()
+        {
+            InitializeComponent();
+        }
+    }
+}
