5954f554dc43deb76f40f13a7cf4bd34b817ae02
m.bazire <m.bazire@zero-gachis.com>
2020-10-28 11:59:27 +0100

    ✨ Add component LabelledDivider

---
+++ Smartway.UiComponent/Separators/LabelledDivider.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Separators
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LabelledDivider
+    {
+        public static readonly BindableProperty LabelProperty = BindableProperty.Create(nameof(Label), typeof(string), typeof(LabelledDivider));
+        public static readonly BindableProperty ColorProperty = BindableProperty.Create(nameof(Color), typeof(string), typeof(LabelledDivider), "#E2E4F0");
+
+        public string Label
+        {
+            get => (string) GetValue(LabelProperty); 
+            set => SetValue(LabelProperty, value);
+        }
+
+        public string Color
+        {
+            get => (string) GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+
+        public LabelledDivider()
+        {
+            InitializeComponent();
+        }
+    }
+}
