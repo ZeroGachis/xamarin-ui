6f86859bb9df2db14f03c2645d48d654bd1dd338
sGeeK <f.perin@zero-gachis.com>
2020-11-03 16:36:36 +0100

    :sparkles: Add android specific plateform

---
+++ Smartway.UiComponent.Droid/Renderers/ZgEntryRenderer.cs
@@ -0,0 +1,46 @@
+﻿using Android.Content;
+using Android.Content.Res;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using Smartway.UiComponent.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Color = Android.Graphics.Color;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(ZgEntryRenderer))]
+namespace Smartway.UiComponent.Droid.Renderers
+{
+    public class ZgEntryRenderer : EntryRenderer
+    {
+        public ZgEntryRenderer(Context context)
+            : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null)
+                return;
+
+            RemoveHintBottomLine();
+            SetCursorColor();
+        }
+
+        private void SetCursorColor()
+        {
+            var intPtrtextViewClass = JNIEnv.FindClass(typeof(TextView));
+            var mCursorDrawableResProperty = JNIEnv.GetFieldID(intPtrtextViewClass, "mCursorDrawableRes", "I");
+            JNIEnv.SetField(Control.Handle, mCursorDrawableResProperty, Resource.Drawable.cursor);
+        }
+
+        private void RemoveHintBottomLine()
+        {
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+                Control.BackgroundTintList = ColorStateList.ValueOf(Color.Transparent);
+            else
+                Control.Background.SetColorFilter(Color.Transparent, PorterDuff.Mode.SrcAtop);
+        }
+    }
+}
