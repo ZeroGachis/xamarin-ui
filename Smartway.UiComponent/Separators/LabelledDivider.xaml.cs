6953bcf0dc5466442df1a3dafd19d564c05580f2
a-bertrand <a.bertrand@zero-gachis.com>
2020-11-04 16:55:24 +0100

    :lipstick: fix separators visual

--- Smartway.UiComponent/Separators/LabelledDivider.xaml.cs
+++ Smartway.UiComponent/Separators/LabelledDivider.xaml.cs
@@ -7,20 +7,12 @@ namespace Smartway.UiComponent.Separators
     public partial class LabelledDivider
     {
         public static readonly BindableProperty LabelProperty = BindableProperty.Create(nameof(Label), typeof(string), typeof(LabelledDivider));
-        public static readonly BindableProperty ColorProperty = BindableProperty.Create(nameof(Color), typeof(string), typeof(LabelledDivider), "#E2E4F0");
 
         public string Label
         {
             get => (string) GetValue(LabelProperty); 
             set => SetValue(LabelProperty, value);
         }
-
-        public string Color
-        {
-            get => (string) GetValue(ColorProperty);
-            set => SetValue(ColorProperty, value);
-        }
-
         public LabelledDivider()
         {
             InitializeComponent();
