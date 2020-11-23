f3ee93193f0541c3072da13b8c6bb95d7381c3c9
valentinmagrez <valentin.magrez@gmail.com>
2020-11-23 21:47:51 +0100

    :sparkles: add nodialog datepicker

---
+++ Smartway.UiComponent.Droid/Renderers/NoDialogDatePickerRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Smartway.UiComponent.Droid.Renderers;
+using Smartway.UiComponent.Inputs;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using DatePickerForms = Xamarin.Forms.DatePicker;
+using DatePickerAndroid = Android.Widget.DatePicker;
+
+[assembly: ExportRenderer(typeof(NoDialogDatePicker), typeof(NoDialogDatePickerRenderer))]
+namespace Smartway.UiComponent.Droid.Renderers
+{
+	public class NoDialogDatePickerRenderer : ViewRenderer<DatePickerForms, DatePickerAndroid>
+	{
+		public NoDialogDatePickerRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePickerForms> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                var dialog = new DatePickerDialog(Context);
+                SetNativeControl(dialog.DatePicker);
+            }
+        }   
+    }
+}
