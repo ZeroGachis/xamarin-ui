72c38ded5bfcc38dc6e568a80991f025c01da7ff
azertyValentin <etu.valentin.menoret@gmail.com>
2020-11-30 10:57:59 +0100

    :sparkles: add custom renderer for disabled button

---
+++ Smartway.UiComponent.Droid/Renderers/CustomButtonRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using Android.Content;
+using Smartway.UiComponent.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using ButtonRenderer = Xamarin.Forms.Platform.Android.FastRenderers.ButtonRenderer;
+
+[assembly: ExportRenderer(typeof(Button), typeof(CustomButtonRenderer))]
+namespace Smartway.UiComponent.Droid.Renderers
+{
+    public class CustomButtonRenderer : ButtonRenderer
+    {
+        public CustomButtonRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> args)
+        {
+            base.OnElementChanged(args);
+            if (Control != null) SetColors();
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(sender, args);
+            if (args.PropertyName == nameof(Button.IsEnabled)) SetColors();
+        }
+
+        private void SetColors()
+        {
+            if(!Element.IsEnabled)
+                Control.SetTextColor(Android.Graphics.Color.White);
+        }
+    }
+}
