ee1d13b453e1c581493ea6c02b2fc3cdbb906396
Thomas IMPERY <t.impery@gmail.com>
2020-12-09 11:09:07 +0100

    :lipstick: Add custom input

---
+++ Smartway.UiComponent.Sample.Android/CustomentryRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.Text;
+using Smartway.UiComponent.FormsElements;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace Smartway.UiComponent.FormsElements
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                GradientDrawable gd = new GradientDrawable();
+                gd.SetColor(global::Android.Graphics.Color.Transparent);
+                this.Control.SetBackgroundDrawable(null);
+                this.Control.SetRawInputType(InputTypes.TextFlagNoSuggestions);
+                Control.SetHintTextColor(ColorStateList.ValueOf(global::Android.Graphics.Color.White));
+            }
+        }
+    }
+}
---
+++ Smartway.UiComponent/FormsElements/CustomEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+using Smartway.UiComponent.FormsElements;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace Smartway.UiComponent.FormsElements
+{
+    public class CustomEntry : Entry
+    {
+
+    }
+}
