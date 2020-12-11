c66f34f6712626b389377f70f32443775c6e318d
sGeeK <f.perin@zero-gachis.com>
2020-12-11 11:00:56 +0100

    :tada: Add radio button

---
+++ Smartway.UiComponent/Inputs/RadioButton.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Inputs
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RadioButton : Frame
+    {
+        public static readonly BindableProperty IsCheckedProperty =
+            BindableProperty.Create(nameof(IsChecked), typeof(bool), typeof(RadioButton), false);
+
+        public bool IsChecked
+        {
+            get => (bool) GetValue(IsCheckedProperty);
+            set => SetValue(IsCheckedProperty, value);
+        }
+
+        public RadioButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Smartway.UiComponent/Inputs/RadioButtonLabel.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Inputs
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RadioButtonLabel
+    {
+        public class CommandParameters
+        {
+            /// <summary>
+            /// The component from where the command has been fired.
+            /// </summary>
+            public RadioButtonLabel Sender { get; set; }
+
+            /// <summary>
+            /// The user defined parameter passed to the command.
+            /// </summary>
+            public object Parameter { get; set; }
+        }
+
+        public static readonly BindableProperty IsCheckedProperty =
+            BindableProperty.Create(nameof(IsChecked), typeof(bool), typeof(RadioButtonLabel));
+        public static readonly BindableProperty LabelProperty =
+            BindableProperty.Create(nameof(Label), typeof(string), typeof(RadioButtonLabel));
+        public static readonly BindableProperty CommandProperty = BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(RadioButtonLabel));
+        public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(RadioButtonLabel));
+        public static readonly BindableProperty IndexProperty = BindableProperty.Create(nameof(Index), typeof(int), typeof(RadioButtonLabel));
+
+        public bool IsChecked
+        {
+            get => (bool)GetValue(IsCheckedProperty);
+            set => SetValue(IsCheckedProperty, value);
+        }
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        public int Index
+        {
+            get => (int)GetValue(IndexProperty);
+            set => SetValue(IndexProperty, value);
+        }
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public ICommand ToggleRadioButton => new Command(() =>
+        {
+            IsChecked = true;
+        });
+
+        public object CommandParameter
+        {
+            get => new CommandParameters{ Sender = this, Parameter = GetValue(CommandParameterProperty)};
+            set => SetValue(CommandParameterProperty, value);
+        }
+
+        public RadioButtonLabel()
+        {
+            InitializeComponent();
+        }
+    }
+}
