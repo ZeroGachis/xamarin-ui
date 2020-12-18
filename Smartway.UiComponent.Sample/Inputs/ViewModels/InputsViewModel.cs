dbcd319694bbefa100c4bd0ef6073e49458c92ec
Valentin Magrez <valentin.magrez@gmail.com>
2020-12-18 21:38:19 +0100

    :bug: Radio buton, IsChecked property can modify the source value

--- Smartway.UiComponent.Sample/Inputs/ViewModels/InputsViewModel.cs
+++ Smartway.UiComponent.Sample/Inputs/ViewModels/InputsViewModel.cs
@@ -1,10 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Smartway.UiComponent.Sample.Inputs.ViewModels
 {
+    public class RadioButtonInputViewModel : ViewModel
+    {
+        public string Label { get; set; }
+
+        public bool IsChecked { get; set; }
+    }
+
     public class InputsViewModel: ViewModel
     {
+        public InputsViewModel()
+        {
+            RadioButtons = new ObservableCollection<RadioButtonInputViewModel>
+            {
+                new RadioButtonInputViewModel{Label = "Vanille", IsChecked = true},
+                new RadioButtonInputViewModel{Label = "Chocolat"}
+            };
+        }
+
+        public ObservableCollection<RadioButtonInputViewModel> RadioButtons { get; set; }
+
+        private RadioButtonInputViewModel SelectedElement => RadioButtons.FirstOrDefault(_ => _.IsChecked);
+
+        public ICommand Validate => new Command(() =>
+        {
+            var message = $"{SelectedElement.Label} selected element";
+            DependencyService.Get<INotifyMessage>().ShortAlert(message);
+        });
     }
 }
