a82099caae2554eae909b14a05373fb0a0c8244d
Azzi <a.azzi@zero-gachis.com>
2021-01-27 16:34:10 +0100

    :lipstick: wrape NoDialogDatePicker into RoundedFrame

---
+++ Smartway.UiComponent/Inputs/RoundedDatePicker.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Inputs
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RoundedDatePicker : ContentView
+    {
+        public RoundedDatePicker()
+        {
+            InitializeComponent();
+        }
+  
+        public static readonly BindableProperty DateProperty =
+            BindableProperty.Create("Date", typeof(DateTime), typeof(RoundedDatePicker));
+
+        public static readonly BindableProperty MinimumDateProperty =
+            BindableProperty.Create(nameof(MinimumDate), typeof(DateTime), typeof(RoundedDatePicker));
+
+        public static readonly BindableProperty MaximumDateProperty =
+            BindableProperty.Create(nameof(MaximumDate), typeof(DateTime), typeof(RoundedDatePicker), new DateTime(2100, 12, 31));
+
+        public DateTime Date
+        {
+            get => (DateTime) GetValue(DateProperty);
+            set => SetValue(DateProperty, value);
+        }
+
+        public DateTime MinimumDate
+        {
+            get => (DateTime)GetValue(MinimumDateProperty);
+            set => SetValue(MinimumDateProperty, value);
+        }
+
+        public DateTime MaximumDate
+        {
+            get => (DateTime)GetValue(MaximumDateProperty);
+            set => SetValue(MaximumDateProperty, value);
+        }
+    }
+}
