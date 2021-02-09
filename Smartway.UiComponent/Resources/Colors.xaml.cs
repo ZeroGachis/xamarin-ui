bf7dcbb4ab4fb4a1cf141c996ce8d4d9d53a1fdc
Kévin <k.lenoir@zero-gachis.com>
2021-02-09 16:11:17 +0100

    💄 Update all code to use Colors.xaml resource

---
+++ Smartway.UiComponent/Resources/Colors.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Resources
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Colors
+    {
+        public Colors()
+        {
+            InitializeComponent();
+        }
+    }
+}
