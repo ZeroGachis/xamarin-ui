92536586a5579465926a2ddd4ab7f88ffa017ba9
Balou <ewen.balouin@imie.fr>
2021-02-09 14:09:35 +0100

    :sparkles: create ClickableItem

---
+++ Smartway.UiComponent/Layouts/ClickableItem.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Layouts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ClickableItem
+    {
+        public static readonly BindableProperty BoxWidthProperty = BindableProperty.Create(nameof(BoxWidth), typeof(int), typeof(ClickableItem));
+        public static readonly BindableProperty BoxHeightProperty = BindableProperty.Create(nameof(BoxHeight), typeof(int), typeof(ClickableItem));
+        public static readonly BindableProperty LayoutContentProperty = BindableProperty.Create(nameof(LayoutContent), typeof(View), typeof(ClickableItem));
+
+        public ClickableItem()
+        {
+            InitializeComponent();
+        }
+        public View LayoutContent
+        {
+            get => (View)GetValue(LayoutContentProperty);
+            set => SetValue(LayoutContentProperty, value);
+        }
+
+        public int BoxWidth
+        {
+            get => (int)GetValue(BoxWidthProperty);
+            set => SetValue(BoxWidthProperty, value);
+        }
+
+        public int BoxHeight
+        {
+            get => (int)GetValue(BoxHeightProperty);
+            set => SetValue(BoxHeightProperty, value);
+        }
+
+    }
+}
