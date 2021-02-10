4e8dff719bcace9d035a8e04546e8e595b1cc845
Kévin <k.lenoir@zero-gachis.com>
2021-02-10 11:46:53 +0100

    🔥 Clean files

--- Smartway.UiComponent.Sample.Android/Resources/Resource.designer.cs
+++ Smartway.UiComponent.Sample.Android/Resources/Resource.designer.cs
@@ -676,6 +676,7 @@ namespace Smartway.UiComponent.Sample.Droid
 			global::Smartway.UiComponent.Droid.Resource.Color.switch_thumb_normal_material_light = global::Smartway.UiComponent.Sample.Droid.Resource.Color.switch_thumb_normal_material_light;
 			global::Smartway.UiComponent.Droid.Resource.Color.tooltip_background_dark = global::Smartway.UiComponent.Sample.Droid.Resource.Color.tooltip_background_dark;
 			global::Smartway.UiComponent.Droid.Resource.Color.tooltip_background_light = global::Smartway.UiComponent.Sample.Droid.Resource.Color.tooltip_background_light;
+			global::Smartway.UiComponent.Droid.Resource.Color.whiteSmoke = global::Smartway.UiComponent.Sample.Droid.Resource.Color.whiteSmoke;
 			global::Smartway.UiComponent.Droid.Resource.Dimension.abc_action_bar_content_inset_material = global::Smartway.UiComponent.Sample.Droid.Resource.Dimension.abc_action_bar_content_inset_material;
 			global::Smartway.UiComponent.Droid.Resource.Dimension.abc_action_bar_content_inset_with_nav = global::Smartway.UiComponent.Sample.Droid.Resource.Dimension.abc_action_bar_content_inset_with_nav;
 			global::Smartway.UiComponent.Droid.Resource.Dimension.abc_action_bar_default_height_material = global::Smartway.UiComponent.Sample.Droid.Resource.Dimension.abc_action_bar_default_height_material;
@@ -990,8 +991,6 @@ namespace Smartway.UiComponent.Sample.Droid
 			global::Smartway.UiComponent.Droid.Resource.Drawable.design_ic_visibility_off = global::Smartway.UiComponent.Sample.Droid.Resource.Drawable.design_ic_visibility_off;
 			global::Smartway.UiComponent.Droid.Resource.Drawable.design_password_eye = global::Smartway.UiComponent.Sample.Droid.Resource.Drawable.design_password_eye;
 			global::Smartway.UiComponent.Droid.Resource.Drawable.design_snackbar_background = global::Smartway.UiComponent.Sample.Droid.Resource.Drawable.design_snackbar_background;
-			global::Smartway.UiComponent.Droid.Resource.Drawable.icon_close_grey = global::Smartway.UiComponent.Sample.Droid.Resource.Drawable.icon_close_grey;
-			global::Smartway.UiComponent.Droid.Resource.Drawable.icon_search = global::Smartway.UiComponent.Sample.Droid.Resource.Drawable.icon_search;
 			global::Smartway.UiComponent.Droid.Resource.Drawable.ic_mtrl_chip_checked_black = global::Smartway.UiComponent.Sample.Droid.Resource.Drawable.ic_mtrl_chip_checked_black;
 			global::Smartway.UiComponent.Droid.Resource.Drawable.ic_mtrl_chip_checked_circle = global::Smartway.UiComponent.Sample.Droid.Resource.Drawable.ic_mtrl_chip_checked_circle;
 			global::Smartway.UiComponent.Droid.Resource.Drawable.ic_mtrl_chip_close_circle = global::Smartway.UiComponent.Sample.Droid.Resource.Drawable.ic_mtrl_chip_close_circle;
@@ -1012,6 +1011,8 @@ namespace Smartway.UiComponent.Sample.Droid
 			global::Smartway.UiComponent.Droid.Resource.Drawable.notify_panel_notification_icon_bg = global::Smartway.UiComponent.Sample.Droid.Resource.Drawable.notify_panel_notification_icon_bg;
 			global::Smartway.UiComponent.Droid.Resource.Drawable.tooltip_frame_dark = global::Smartway.UiComponent.Sample.Droid.Resource.Drawable.tooltip_frame_dark;
 			global::Smartway.UiComponent.Droid.Resource.Drawable.tooltip_frame_light = global::Smartway.UiComponent.Sample.Droid.Resource.Drawable.tooltip_frame_light;
+			global::Smartway.UiComponent.Droid.Resource.Font.open_sans = global::Smartway.UiComponent.Sample.Droid.Resource.Font.open_sans;
+			global::Smartway.UiComponent.Droid.Resource.Font.open_sans_regular = global::Smartway.UiComponent.Sample.Droid.Resource.Font.open_sans_regular;
 			global::Smartway.UiComponent.Droid.Resource.Id.action0 = global::Smartway.UiComponent.Sample.Droid.Resource.Id.action0;
 			global::Smartway.UiComponent.Droid.Resource.Id.actions = global::Smartway.UiComponent.Sample.Droid.Resource.Id.actions;
 			global::Smartway.UiComponent.Droid.Resource.Id.action_bar = global::Smartway.UiComponent.Sample.Droid.Resource.Id.action_bar;
@@ -10939,12 +10940,6 @@ namespace Smartway.UiComponent.Sample.Droid
 			// aapt resource value: 0x7F070066
 			public const int icon_check = 2131165286;
 			
-			// aapt resource value: 0x7F070067
-			public const int icon_close_grey = 2131165287;
-			
-			// aapt resource value: 0x7F070068
-			public const int icon_search = 2131165288;
-			
 			// aapt resource value: 0x7F070063
 			public const int ic_mtrl_chip_checked_black = 2131165283;
 			
@@ -10954,56 +10949,56 @@ namespace Smartway.UiComponent.Sample.Droid
 			// aapt resource value: 0x7F070065
 			public const int ic_mtrl_chip_close_circle = 2131165285;
 			
+			// aapt resource value: 0x7F070067
+			public const int mtrl_snackbar_background = 2131165287;
+			
+			// aapt resource value: 0x7F070068
+			public const int mtrl_tabs_default_indicator = 2131165288;
+			
 			// aapt resource value: 0x7F070069
-			public const int mtrl_snackbar_background = 2131165289;
+			public const int navigation_empty_icon = 2131165289;
 			
 			// aapt resource value: 0x7F07006A
-			public const int mtrl_tabs_default_indicator = 2131165290;
+			public const int notification_action_background = 2131165290;
 			
 			// aapt resource value: 0x7F07006B
-			public const int navigation_empty_icon = 2131165291;
+			public const int notification_bg = 2131165291;
 			
 			// aapt resource value: 0x7F07006C
-			public const int notification_action_background = 2131165292;
+			public const int notification_bg_low = 2131165292;
 			
 			// aapt resource value: 0x7F07006D
-			public const int notification_bg = 2131165293;
+			public const int notification_bg_low_normal = 2131165293;
 			
 			// aapt resource value: 0x7F07006E
-			public const int notification_bg_low = 2131165294;
+			public const int notification_bg_low_pressed = 2131165294;
 			
 			// aapt resource value: 0x7F07006F
-			public const int notification_bg_low_normal = 2131165295;
+			public const int notification_bg_normal = 2131165295;
 			
 			// aapt resource value: 0x7F070070
-			public const int notification_bg_low_pressed = 2131165296;
+			public const int notification_bg_normal_pressed = 2131165296;
 			
 			// aapt resource value: 0x7F070071
-			public const int notification_bg_normal = 2131165297;
+			public const int notification_icon_background = 2131165297;
 			
 			// aapt resource value: 0x7F070072
-			public const int notification_bg_normal_pressed = 2131165298;
+			public const int notification_template_icon_bg = 2131165298;
 			
 			// aapt resource value: 0x7F070073
-			public const int notification_icon_background = 2131165299;
+			public const int notification_template_icon_low_bg = 2131165299;
 			
 			// aapt resource value: 0x7F070074
-			public const int notification_template_icon_bg = 2131165300;
+			public const int notification_tile_bg = 2131165300;
 			
 			// aapt resource value: 0x7F070075
-			public const int notification_template_icon_low_bg = 2131165301;
+			public const int notify_panel_notification_icon_bg = 2131165301;
 			
 			// aapt resource value: 0x7F070076
-			public const int notification_tile_bg = 2131165302;
+			public const int tooltip_frame_dark = 2131165302;
 			
 			// aapt resource value: 0x7F070077
-			public const int notify_panel_notification_icon_bg = 2131165303;
-			
-			// aapt resource value: 0x7F070078
-			public const int tooltip_frame_dark = 2131165304;
-			
-			// aapt resource value: 0x7F070079
-			public const int tooltip_frame_light = 2131165305;
+			public const int tooltip_frame_light = 2131165303;
 			
 			static Drawable()
 			{
