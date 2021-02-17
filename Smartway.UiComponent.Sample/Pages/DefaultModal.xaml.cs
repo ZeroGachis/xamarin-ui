f2f664e6410f08240bf1c1f1b598588758f951cc
amelazzi <a.azzi@smartway.ai>
2021-02-17 15:32:47 +0100

    :sparkles: add default modal

---
+++ Smartway.UiComponent.Sample/Pages/DefaultModal.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+using Smartway.UiComponent.Pages;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Sample.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DefaultModal : Modal
+    {
+        public DefaultModal()
+        {
+            InitializeComponent();
+        }
+
+        public ICommand CancelCommand => new Command(async () =>
+        {
+            await Navigation.PopModalAsync(false);
+        });
+    }
+}
