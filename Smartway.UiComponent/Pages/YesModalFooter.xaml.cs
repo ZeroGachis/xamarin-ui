d08be06ea9fa5a802bd25786b831ef4d9a5b3be1
amelazzi <a.azzi@smartway.ai>
2021-02-17 11:20:17 +0100

    :lipstick: add YesModalFooter

---
+++ Smartway.UiComponent/Pages/YesModalFooter.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class YesModalFooter : ContentView
+    {
+        public static readonly BindableProperty ApplyCommandProperty =
+            BindableProperty.Create(nameof(ApplyCommand), typeof(ICommand), typeof(YesModalFooter));
+
+        public ICommand ApplyCommand
+        {
+            get => (ICommand)GetValue(ApplyCommandProperty);
+            set => SetValue(ApplyCommandProperty, value);
+        }
+
+        public YesModalFooter()
+        {
+            InitializeComponent();
+        }
+    }
+}
