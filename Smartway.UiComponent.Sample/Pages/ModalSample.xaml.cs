a03bf471adb24df9fc1081844b7da2e34cede077
amelazzi <a.azzi@smartway.ai>
2021-02-17 15:40:45 +0100

    :sparkles: use ModalStack for modal navigation

--- Smartway.UiComponent.Sample/Pages/ModalSample.xaml.cs
+++ Smartway.UiComponent.Sample/Pages/ModalSample.xaml.cs
@@ -1,14 +1,20 @@
-﻿using Smartway.UiComponent.Pages;
+﻿using System.Windows.Input;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Smartway.UiComponent.Sample.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ModalSample : Modal
+    public partial class ModalSample : ContentPage
     {
         public ModalSample()
         {
             InitializeComponent();
         }
+
+        public ICommand DefaultModal => new Command(async () =>
+        {
+            await this.Navigation.PushModalAsync(new DefaultModal(), false);
+        });
     }
 }
