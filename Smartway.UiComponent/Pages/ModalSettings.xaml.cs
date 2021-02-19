a8d26a4b57d54ee4a0893f1a96ab64f66f20a5bc
amelazzi <a.azzi@smartway.ai>
2021-02-19 10:52:42 +0100

    :sparkles: add Modal Settings

---
+++ Smartway.UiComponent/Pages/ModalSettings.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ModalSettings : Modal
+    {
+
+        public ICommand CancelCommand => new Command(async () =>
+        {
+            await Navigation.PopModalAsync(false);
+        });
+
+        public ModalSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
