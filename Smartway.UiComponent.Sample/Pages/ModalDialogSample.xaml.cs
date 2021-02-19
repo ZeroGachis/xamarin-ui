6c4c930c8d92ac47fb3961b7f312a8715d7b2416
amelazzi <a.azzi@smartway.ai>
2021-02-19 13:10:25 +0100

    :sparkles: add Modal Dialog Sample

---
+++ Smartway.UiComponent.Sample/Pages/ModalDialogSample.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Sample.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ModalDialogSample
+    {
+        public ModalDialogSample()
+        {
+            InitializeComponent();
+        }
+    }
+}
