709cb4796475163f96fbfc8d6a03e2c63b4b9cf5
amelazzi <a.azzi@smartway.ai>
2021-02-19 11:43:11 +0100

    :sparkles: add Modal Dialog

---
+++ Smartway.UiComponent/Pages/ModalDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ModalDialog : Modal
+    {
+        public static readonly BindableProperty MessageProperty =
+            BindableProperty.Create(nameof(Message), typeof(string), typeof(ModalDialog));
+
+        public string Message
+        {
+            get => (string)GetValue(MessageProperty);
+            set => SetValue(MessageProperty, value);
+        }
+
+        public ICommand CancelCommand => new Command(async () =>
+        {
+            await Navigation.PopModalAsync(false);
+        });
+
+        public ModalDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
