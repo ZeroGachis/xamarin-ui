f4e99eceaf0a07a239772549019bafe1f84e9458
amelazzi <a.azzi@smartway.ai>
2021-02-24 17:22:47 +0100

    :lipstick: add Modal Renderer

---
+++ Smartway.UiComponent.Droid/Renderers/ModalRenderer.cs
@@ -0,0 +1,58 @@
+﻿using Android.Content;
+using Android.Views;
+using System;
+using Smartway.UiComponent.Droid.Renderers;
+using Smartway.UiComponent.Pages;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Color = Android.Graphics.Color;
+
+[assembly: ExportRenderer(typeof(Modal), typeof(ModalRenderer))]
+namespace Smartway.UiComponent.Droid.Renderers
+{
+    class ModalRenderer : PageRenderer
+    {
+        private Color statusBarColor;
+        public ModalRenderer(Context context)
+            : base(context) { }
+
+        protected override void OnAttachedToWindow()
+        {
+            Element.Appearing += OnAppearing;
+            Element.Disappearing += OnDisappearing;
+        }
+
+        private void OnAppearing(object sender, EventArgs e)
+        {
+            var currentWindow = GetCurrentWindow();
+            statusBarColor = new Color(currentWindow?.StatusBarColor ?? 0);
+
+            currentWindow?.SetStatusBarColor(Color.Argb(140, 0, 0, 0));
+        }
+
+        private void OnDisappearing(object sender, EventArgs e)
+        {
+            var currentWindow = GetCurrentWindow();
+            currentWindow?.SetStatusBarColor(statusBarColor);
+        }
+
+        private Window GetCurrentWindow()
+        {
+            try
+            {
+                var window = Context.GetActivity().Window;
+                window?.ClearFlags(WindowManagerFlags.TranslucentStatus);
+                window?.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+                return window;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return null;
+
+        }
+    }
+    
+}
