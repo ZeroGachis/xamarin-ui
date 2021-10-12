1d450dedcb5fad48bed03caf0631ad0f40cb4957
amelazzi <a.azzi@smartway.ai>
2021-10-12 11:19:53 +0200

    :sparkles::lipstick: add icons view

---
+++ Smartway.UiComponent.Sample/Icons/Views/Icons.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Smartway.UiComponent.Labels.Icons;
+using Xamarin.Forms.Xaml;
+
+namespace Smartway.UiComponent.Sample.Icons.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Icons
+    {
+        public Icons()
+        {
+            InitializeComponent();
+        }
+
+        public Array IconNames => Enum.GetValues(typeof(Icon.IconNames));
+    }
+}
