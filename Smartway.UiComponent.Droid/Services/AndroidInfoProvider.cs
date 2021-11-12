c5f9b708b79c621a492a4bac2abe8cbbb0016ced
sGeeK <f.perin@zero-gachis.com>
2021-11-12 11:47:35 +0100

    :sparkles: Make Datepicker compatible with Android 6

---
+++ Smartway.UiComponent.Droid/Services/AndroidInfoProvider.cs
@@ -0,0 +1,12 @@
+﻿using Android.OS;
+using Smartway.UiComponent.Droid.Services;
+using Smartway.UiComponent.Services;
+
+[assembly: Xamarin.Forms.Dependency(typeof(AndroidInfoProvider))]
+namespace Smartway.UiComponent.Droid.Services
+{
+    public class AndroidInfoProvider : IPlatormInfoProvider
+    {
+        public int OsSdkVersion { get { return ((int)Build.VERSION.SdkInt); } }
+    }
+}
