72693696fbd64afc1e0131514f804e1f5f0fd59d
Audrey Brouard <a.brouard@smartway.ai>
2021-11-25 12:11:00 +0100

    :sparkles: :lipstick: Add a custom entry without soft keyboard displayed

---
+++ Smartway.UiComponent.Droid/Renderers/EntryWithoutSoftKeyboardRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using Android.Content;
+using Smartway.UiComponent.Droid.Renderers;
+using Smartway.UiComponent.Inputs;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(EntryWithoutSoftKeyboard), typeof(EntryWithoutSoftKeyboardRenderer))]
+namespace Smartway.UiComponent.Droid.Renderers
+{
+    public class EntryWithoutSoftKeyboardRenderer : CustomEntryRenderer
+    {
+        public EntryWithoutSoftKeyboardRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> args)
+        {
+            base.OnElementChanged(args);
+            Control.ShowSoftInputOnFocus = false;
+        }
+    }
+}
