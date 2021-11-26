ecceee5835553fa5c45437612e524282c69677e4
Audrey Brouard <a.brouard@smartway.ai>
2021-11-26 12:45:25 +0100

    :lipstick: hide soft keyboard in numeric input component when force focus

--- Smartway.UiComponent.Droid/Renderers/EntryWithoutSoftKeyboardRenderer.cs
+++ Smartway.UiComponent.Droid/Renderers/EntryWithoutSoftKeyboardRenderer.cs
@@ -1,5 +1,5 @@
-﻿using System.ComponentModel;
-using Android.Content;
+﻿using Android.Content;
+using Android.Views.InputMethods;
 using Smartway.UiComponent.Droid.Renderers;
 using Smartway.UiComponent.Inputs;
 using Xamarin.Forms;
@@ -15,7 +15,42 @@ namespace Smartway.UiComponent.Droid.Renderers
         protected override void OnElementChanged(ElementChangedEventArgs<Entry> args)
         {
             base.OnElementChanged(args);
+            if (args.NewElement != null)
+            {
+                ((EntryWithoutSoftKeyboard)args.NewElement).PropertyChanging += HideKeyboardIfPoppedWhenGettingFocus;
+            }
+
+            if (args.OldElement != null)
+            {
+                ((EntryWithoutSoftKeyboard)args.OldElement).PropertyChanging -= HideKeyboardIfPoppedWhenGettingFocus;
+            }
+
             Control.ShowSoftInputOnFocus = false;
+        }
+
+        private void HideKeyboardIfPoppedWhenGettingFocus(object sender,
+            PropertyChangingEventArgs propertyChangingEventArgs)
+        {
+            var isGettingFocus = propertyChangingEventArgs.PropertyName == VisualElement.IsFocusedProperty.PropertyName;
+            if (isGettingFocus)
+            {
+                InputMethodManager imm = (InputMethodManager)Context.GetSystemService(Context.InputMethodService);
+                imm.HideSoftInputFromWindow(Control.WindowToken, HideSoftInputFlags.None);
+            }
+        }
+
+        protected override void OnFocusChangeRequested(object sender, VisualElement.FocusRequestArgs e)
+        {
+            if (!e.Focus)
+            {
+                base.OnFocusChangeRequested(sender, e);
+                return;
+            }
+
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                Control?.RequestFocus();
+            });
         }
     }
 }
