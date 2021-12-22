6f9f34f1e0b7640b9496ce396e94d635e31c99b9
a-bertrand <a.bertrand@zero-gachis.com>
2021-12-23 00:04:25 +0100

    initial commit

---
+++ Smartway.UiComponent.Sample.Ios/AppDelegate.cs
@@ -0,0 +1,24 @@
+﻿using Foundation;
+using UIKit;
+
+namespace Smartway.UiComponent.Sample.Ios
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register ("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+
+        public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+        {
+            // Override point for customization after application launch.
+            // If not required for your application you can safely delete this method
+
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(application, launchOptions);
+        }
+    }
+}
+
