e21f3a6bcbd2d3447a77b5619435e5d19ba7afd5
Josselin TILLAY <j.tillay@smartway.ai>
2024-03-18 16:03:33 +0100

    :sparkles: Numeric Date Entry - enable custom culture

---
+++ Smartway.UiComponent.UnitTests/Inputs/NumericDateInput/TestableNumericDateInput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Smartway.UiComponent.Inputs;
+
+namespace Smartway.UiComponent.UnitTests.Inputs.NumericDateInput
+{
+    class TestableNumericDateEntry: NumericDateEntry
+    {
+        public new DateTime GetFilledDate() => base.GetFilledDate();
+    }
+}
